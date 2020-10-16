5721798d4a25e64631768f423e36f16951f38d1f
Lucas Araujo Pereira <lucas.araujo.pereira@gmail.com>
2020-10-16 15:16:14 -0300

    formatação

--- react-ui/src/Socket.js
+++ react-ui/src/Socket.js
@@ -1,10 +1,10 @@
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 let socket;
 export const person = Math.random();
 
 const ENDPOINT = {
-  path: '/socket.io',
+  path: "/socket.io",
 
   secure: true,
 };
@@ -13,7 +13,7 @@ export const initiateSocket = (room) => {
   socket = io(ENDPOINT);
   // console.log(`Connecting socket...`);
   if (socket && room) {
-    socket.emit('join', room);
+    socket.emit("join", room);
   }
 };
 export const disconnectSocket = () => {
@@ -23,7 +23,7 @@ export const disconnectSocket = () => {
 
 export const subscribeToChat = (cb) => {
   if (!socket) return true;
-  socket.on('chat', (msg) => {
+  socket.on("chat", (msg) => {
     //  console.log('Websocket event received!');
     return cb(null, msg);
   });
@@ -31,7 +31,7 @@ export const subscribeToChat = (cb) => {
 
 export const subscribeToError = (cb) => {
   if (!socket) return true;
-  socket.on('connect_error', (msg) => {
+  socket.on("connect_error", (msg) => {
     //  console.log('Websocket event received!');
     return cb(null, msg);
   });
@@ -39,19 +39,19 @@ export const subscribeToError = (cb) => {
 
 export const subscribeToReconnect = (cb) => {
   if (!socket) return true;
-  socket.on('reconnect', (msg) => {
+  socket.on("reconnect", (msg) => {
     //  console.log('Websocket event received!');
     return cb(null, msg);
   });
 };
 
 export const sendMessage = (room, message) => {
-  if (socket) socket.emit('chat', { message, room, person });
+  if (socket) socket.emit("chat", { message, room, person });
 };
 
 export const subscribeToQtd = (cb) => {
   if (!socket) return true;
-  socket.on('qtdConectado', (msg) => {
+  socket.on("qtdConectado", (msg) => {
     //   console.log('Websocket event received!', msg);
     return cb(null, msg);
   });
