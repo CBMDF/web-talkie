43baae5d581ae340cb6deaab62604e527842275b
Lucas Araujo Pereira <lucas.araujo.pereira@gmail.com>
2020-10-19 16:18:57 -0300

    Compression

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cluster = require("cluster");
 const numCPUs = require("os").cpus().length;
+var compression = require("compression");
 
 const socketIo = require("socket.io");
 const isDev = process.env.NODE_ENV !== "production";
@@ -23,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 // } else {
 const app = express();
 const server = require("http").createServer(app);
-
+app.use(compression());
 const io = socketIo(server);
 
 app.use(function (req, res, next) {
